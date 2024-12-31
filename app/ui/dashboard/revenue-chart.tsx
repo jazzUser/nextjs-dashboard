c1849ad9ef3a827266ad25e0cbca3a49d1934247
Jasmina Muharemovic <jasminamhrmvc@proton.me>
2024-12-31 13:52:45 +0100

    finish chapter 9

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -2,6 +2,7 @@ import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts'
 import { Revenue } from '@/app/lib/definitions';
+import { fetchRevenue } from '@/app/lib/data';
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -9,18 +10,15 @@ import { Revenue } from '@/app/lib/definitions';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue(); // Fetch data inside the component
+ 
   const chartHeight = 350;
-
-   const { yAxisLabels, topLabel } = generateYAxis(revenue);
-
+  const { yAxisLabels, topLabel } = generateYAxis(revenue);
+ 
   if (!revenue || revenue.length === 0) {
     return <p className="mt-4 text-gray-400">No data available.</p>;
-   }
+  }
 
   return (
     <div className="w-full md:col-span-4">
@@ -33,9 +31,9 @@ export default async function RevenueChart({
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
             style={{ height: `${chartHeight}px` }}
           >
-            {yAxisLabels.map((label) => (
+          {/*   {yAxisLabels.map((label) => (
               <p key={label}>{label}</p>
-            ))}
+            ))} */}
           </div>
 
           {revenue.map((month) => (
